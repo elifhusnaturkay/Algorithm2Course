d58c80cd9371299025c7a67527eb97a2fd9c5b35
Elif Hüsna Turkay <elifhusnaturkay@hotmail.com>
2024-03-19 11:51:34 +0300

    5 march 2024, first course

---
+++ Chapter-1-The-way-of-the-program-Elif-HusnaHusna/main.py
@@ -0,0 +1,9 @@
+# Write your code here
+print("Hello,World!")
+#Isn't this easy!
+
+num1 = int(input('Enter a number:'))
+num2 = input('Enter another number:')
+sum = num1 + int(num2)
+sum1 = num1 * num2
+print('The sum of',num1,'and', num2,'is',sum, 'and',sum1)
